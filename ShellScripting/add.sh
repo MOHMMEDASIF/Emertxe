2ec147c5f848290764b929b578ad2fdd70f3974b
Emertxe <asifsalam00@gmail.com>
2017-08-24 01:35:40 +0530

    Updated ShellScripting

--- ShellScripting/add.sh
+++ ShellScripting/add.sh
@@ -1,11 +1,42 @@
-#Program takes two number and adds them using 3 different methods
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------Expected Solution -------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+#Program takes two number and adds them using 3 different methods without command line argument
 #contains Switch Statement, else if statement to validate the input method to add two numbers of the user 
 
+<<ExpectedInput
+	Enter the value of number1: 2
+	Enter the value of number2: 2
+	Enter the method to be used in Addition operation 
+         method 1 uses expr command to add two numbers
+         method 2 uses bc (floating point is accepted) command to add two numbers" 
+         method 3 uses (( )) command to add two numbers"
+	2
+
+
+
+ExpectedInput
+
+
+<<ExpectedOutput
+	
+	
+	2+2=4	
+
+
+
+ExpectedOutput
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------Begining of the Program ----------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
 #!/bin/bash
 
 
 #prompt the user to enter the value of Number 1
-	printf "Enter the value of num1 " #printf doesnt gives the newline space while its is available in echo
+printf "Enter the value of num1 " #printf doesnt gives the newline space while its is available in echo
 #read and  store the value of num1
 	read num1
 
@@ -16,6 +47,9 @@
 	read num2
 #prompt the user to get which method to use to add rwo numbers
 	echo "Enter the method to be used in Addition operation "
+	echo "method 1 uses expr command to add two numbers"
+	echo "method 2 uses bc (floating point is accepted) command to add two numbers" 
+	echo "method 3 uses (( )) command to add two numbers"
 #read the user input from the promp and store it in the var
 	read method 
 
@@ -27,7 +61,7 @@
 	#using method 2(basic calculator)
 	#sum=`echo "$num1 + $num2" | bc `
 	#sum=$(bc <<< "$num1+$num2")
-		#sum=$((`bc <<< $num1+$num2`))
+	#sum=$((`bc <<< $num1+$num2`))
 
 	#using method 3
 	#sum=$(($num1+$num2))
@@ -42,7 +76,7 @@
 #methods to use in adding two numbers using switch statement
 case "$method" in 
 	"1") sum=`expr $num1 + $num2` ;;
-	"2") sum=$((`bc <<< $num1+$num2`));;
+	"2") sum=$((`bc<<<$num1+$num2`));; #sum=$(bc <<< "$num1+$num2")
 	"3") sum=$(($num1+$num2));;
 	 *)  printf "invalid input & " ;;
 esac
@@ -54,3 +88,7 @@ then
 else
 	 echo "use valid method to add two numbers"
 fi
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------End of the Program --------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
--- ShellScripting/arithematic.sh
+++ ShellScripting/arithematic.sh
@@ -1,5 +1,97 @@
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------ Expected solution ----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
 #This program is an example to perform Arithematic operation (+,-,*,/,%)
 
+<<ExpectedInput
+	syntax:-	bash filename.sh operator1 operant operator2
+	example:-	bash arithematic.sh 12 + 2
+ExpectedInput
+
+<<ExpectedOutput
+	syntax:-	Arithematic Operation Performed is operant
+			operator1+operator2=result	
+	example:-	Arithematic operation performed is +
+			12+2=14
+ExpectedOutput
+
+
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------- Starting of the Program -------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #!/bin/bash
 
+
+
+#below statement validates the count of the argument
+if [ "$#" -eq 3 ]
+then 
+	operator1=$1	#argument one is passed to an variable operator1
+	operator2=$3	#argument three  is passed to an variable operator2
+	operant=$2	#argument two is passed to an variable operant
+
+
+
+
+#case statement identifies "which operant it belongs to in arithematic operation"
+case "$operant" in
+	
+	"+") result=`echo "$operator1 + $operator2" | bc`;;
+	"-") result=`echo "$operator1 - $operator2" | bc`;;
+	"/") result=`echo "$operator1 / $operator2" | bc`;;
+	"*") result=`echo "$operator1 * $operator2" | bc`;;
+	"%") result=`echo "$operator1 % $operator2" | bc`;;
+	*)   echo "Invalid operant "
+	     echo " Try with new operant" 
+	         exit ;;
+ 
+esac
+
+
+
+#prints the result in the terminal with oppropriate message
+echo "Arithematic operation performed is $operant"
+echo "Therefore $operator1$operant$operator2=$result"
+
+
+
+
+
+
+#prompt the user to continue or to exit and read the value of the user in a variable
+#<<doubt
+#how to perform the continues operation by taking argument as an input and invoking the 
+echo "Do you want to continue?[Y/N]"
+read input
+
+if [ $input = "Y" ]
+then 
+	echo "Enter the expression {syntax:-operator1 operant operator2 e.g:1 + 2}"
+	read arguments
+	bash arithematic.sh $arguments
+else
+	echo "Program has stopped executing"
+	echo "Thank you For using Arithematic operation ;-)"
+	exit
+fi
+
+#doubt (doubt was cleared by trying out new ways)
+
+
+#prompt the user by saying to pass an argument
+else
+	echo "Re-try with \"bash filename.sh operator1 operant operator2\" "
+	echo "e.g:bash filename.sh 1 + 2 "
+	echo "to perform the operation"
+fi
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------End of the program -------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
